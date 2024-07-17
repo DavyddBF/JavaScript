c704d46f7abdf9e763007bb7801e8022be417479
DavyddBF <davydextreme460@gmail.com>
2024-07-17 10:30:24 -0300

    Adicionando diretório do html

--- firebaseJS/src/app/app.js
+++ firebaseJS/src/app/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
@@ -6,9 +7,9 @@ const azul = '\x1b[34m\x1b[4m';
 const reset = '\x1b[0m';
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Servidor aberto na porta ${azul}http://localhost:${ port }/${reset}`);
+    console.log(`\nServidor aberto na porta ${azul}http://localhost:${ port }/${reset}`);
 });
