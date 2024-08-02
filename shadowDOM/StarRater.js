f75c4a40eaa7999f72fc52b1cc0c2d028f389853
DavyddBF <davydextreme460@gmail.com>
2024-08-01 22:02:40 -0300

    Criando coisas aleatórias | Testando...

--- shadowDOM/StarRater.js
+++ shadowDOM/StarRater.js
@@ -1,2 +1,41 @@
 import './style.css';
 
+class StarRater extends HTMLElement {
+  constructor() {
+    super()
+
+    this.build();
+  }
+
+  build() {
+    this.styleCss();
+
+    this.appDiv();
+  }
+
+  styleCss() {
+    const linkCss = document.createElement('link');
+    linkCss.setAttribute('rel', 'stylesheet');
+    linkCss.setAttribute('href', './teste.css');
+
+    document.querySelector('head').appendChild(linkCss);
+  }
+
+  appDiv() {
+    const app = document.getElementById('app');
+    const appDiv = document.createElement('div');
+    appDiv.classList.add('div-class');
+    app.appendChild(appDiv);
+
+
+    const titulo = document.createElement('h1');
+    titulo.textContent = 'Olá Mundo!';
+
+    const div = document.querySelector('.div-class');
+    div.appendChild(titulo);
+
+    return app;
+  }
+}
+
+customElements.define('star-rater', StarRater)
