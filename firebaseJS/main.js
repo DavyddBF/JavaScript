8e5009e944b15aadb0a89ef1decad8f7dc39d89c
DavyddBF <davydextreme460@gmail.com>
2024-08-01 21:32:03 -0300

    Criando funções

--- firebaseJS/main.js
+++ firebaseJS/main.js
@@ -9,7 +9,8 @@ import {
   from 'firebase/firestore';
   import { 
     createUserWithEmailAndPassword,
-    signInWithEmailAndPassword 
+    signInWithEmailAndPassword, 
+    signOut
   } from 'firebase/auth';
   import { db, auth } from './public/firebase/firebaseConnection';
 
@@ -88,18 +89,56 @@ loginBtn.addEventListener('click', loginUsuario)
 cadastrarUserBtn.addEventListener('click', cadastrar);
 buscarUsersBtn.addEventListener('click', buscarTodosUsers);
 
+async function carregarUser() {
+  const unsub = onSnapshot(collection(db, 'user'), (snapshot) => {
+    let listaUser = [];
+
+    snapshot.forEach((cadaUser) => {
+      listaUser.push({
+        id: cadaUser.id,
+        user: cadaUser.data().user,
+        idade: cadaUser.data().idade
+      });
+    });
+
+    users = listaUser;
+    renderizaUser();
+    renderFilteredUsers();
+  });
+
+  return () => unsub();
+}
+
+async function checarLogin() {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      detalheUser = {
+        uid: user.uid,
+        email: user.email
+      };
+      loginUser = true;
+      renderLogin();
+    } else {
+      detalheUser = {};
+      loginUser = false;
+      renderLogin();
+    }
+  });
+}
+
 async function novoUsuario() {
   await createUserWithEmailAndPassword(auth, email, senha)
   .then(() => {
     console.log('Cadastrado com sucesso!');
+    email = '';
+    senha = '';
     emailInput.value = '';
     senhaInput.value = '';
   })
   .catch((erro) => {
-    if(erro.code == 'auth/weak-password'){
-
+    if(erro.code == 'auth/weak-password') {
       alert('Senha muito fraca!!');
-    } else if (erro.code == 'auth/email-already-in-use') {
+    } else if(erro.code == 'auth/email-already-in-use') {
       alert('Email já existe!!');
     }
   })
@@ -117,13 +156,33 @@ async function loginUsuario() {
     }
 
     loginUser = true;
+    renderLogin()
 
+    email = '';
+    senha = '';
     emailInput.value = '';
     senhaInput.value = '';
   })
   .catch(() => {
     console.log('Não foi possível fazer o login!!');
   });
+}
+
+async function fazerLogout() {
+  await signOut(auth);
+  loginUser = false;
+  detalheUser = {};
+  renderLogin();
+}
+
+function renderLogin() {
+  if(loginUser) {
+    loggedInContainer.style.display = 'block';
+    userDetails.textContent  = `Id: ${detalheUser.uid} - Email: ${detalheUser.email}`;
+  } else {
+    loggedInContainer.style.display = 'none';
+    userDetails.textContent = '';
+  }
 }
 
 async function cadastrar() {
@@ -134,6 +193,8 @@ async function cadastrar() {
   })
   .then(() => {
     console.log('Deu tudo certo!!');
+    user = '';
+    idade = '';
     userInput.value = '';
     idadeInput.value = '';
   })
@@ -158,6 +219,7 @@ async function buscarTodosUsers() {
 
     users = lista;
     renderizaUser();
+    renderFilteredUsers();
   })
   .catch((erro) => {
     console.log('Houve um erro!! ' + erro)
@@ -169,6 +231,10 @@ async function excluirUser(id) {
   await deleteDoc(refDoc)
   .then(() => {
     console.log('User deletado com sucesso!!')
+    buscarTodosUsers();
+  })
+  .catch((erro) => {
+    console.log('Houve um erro ao deletar!! ' + erro);
   });
 }
 
@@ -180,6 +246,8 @@ async function atualizarUser(id) {
   })
   .then(() => {
     console.log('Edição feita com sucesso!!');
+    user = '';
+    idade = '';
     userInput.value = '';
     idadeInput.value = '';
   })
@@ -192,12 +260,40 @@ function renderizaUser() {
   document.getElementById('listaUser').innerHTML = users.map((user) => {
     return `
       <li>
-        <strong>ID: ${user.id}</strong>
-        <span>User: ${user.user}</span>
-        <span>Idade: ${user.idade}</span>
-        <button id="excluirUserBtn" onclick="excluirUser('${user.id}')">Excluir</button>
-        <button id="atualizarUserBtn" onclick="atualizarUser('${user.id}')">Editar</button>
+        <strong>ID: ${user.id}</strong> </br>
+        <span>User: ${user.user}</span> </br>
+        <span>Idade: ${user.idade}</span> </br>
+        <button class="excluirUserBtn" data-id="${user.id}">Excluir</button>
+        <button class="atualizarUserBtn" data-id="${user.id}">Editar</button>
       </li>
     `;
-  })
+  }).join('');
+
+  const excluirUserBtn = document.querySelectorAll('excluirUserBtn');
+  const atualizarUserBtn = document.querySelectorAll('atualizarUserBtn');
+
+  excluirUserBtn.forEach((btn) => {
+    btn.addEventListener('click', (evento) => {
+      const userID = document.querySelector('[data-id]');
+      excluirUser(userID);
+    });
+  });
+
+  atualizarUserBtn.forEach((btn) => {
+    btn.addEventListener('click', (evento) => {
+      const userID = evento.target.getAttribute('data-id');
+      atualizarUser(userID);
+    });
+  });
+}
+
+function renderFilteredUsers() {
+  const idadeFiltrada = users.filter(user => Number(user.idade) > 20);
+  idadeFiltradaList.innerHTML = idadeFiltrada.map((maioresVinte) => `
+    <li>
+      <strong>ID: ${maioresVinte.id}</strong> <br/>
+      <span>User: ${maioresVinte.user}</span> <br/>
+      <span>Idade: ${maioresVinte.idade}</span> <br/> <br/>
+    </li>
+  `).join('');
 }
